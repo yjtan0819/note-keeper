7cb8e7050f968970bd60794f3a05f021f70f8c43
yjtan0819 <tan.yijia0819@gmail.com>
2023-05-28 23:49:31 -0400

    Add and delete functionalities

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
 function Footer() {
-    var currentYear = new Date().getFullYear();
-    return (
-        <footer>
-        <p>CopyRight © {currentYear}</p>
-        </footer>
-    );
-    }
+  const year = new Date().getFullYear();
+  return (
+    <footer>
+      <p>Copyright ⓒ {year}</p>
+    </footer>
+  );
+}
 
 export default Footer;
